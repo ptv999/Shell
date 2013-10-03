a2f48e6dfbed011466d97b11a8df3e143c66aeef
ptv999 <Duke@wl-10-190-214-40.wireless.duke.edu>
2013-10-02 21:01:30 -0400

    Initial Commit

---
+++ Shell/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Shell
+//
+//  Created by Duke Kim on 10/2/13.
+//  Copyright (c) 2013 Duke Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
